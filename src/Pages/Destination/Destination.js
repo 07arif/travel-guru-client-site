445ab5b79dbd8e61b2bb6d650c85ce0712c93ee6
07arif <ai.arif0711@gmail.com>
2023-01-04 13:17:57 +0600

    first commit

---
+++ src/Pages/Destination/Destination.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Destination = () => {
+    return (
+        <div>
+            <p>THis is Destination</p>
+        </div>
+    );
+};
+
+export default Destination;
---
+++ src/Pages/Shared/Others/TermsAndCondition/TermsAndCondition.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TermsAndCondition = () => {
+    return (
+        <div>
+            <p>This is term section</p>
+        </div>
+    );
+};
+
+export default TermsAndCondition;
